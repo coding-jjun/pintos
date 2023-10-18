458cd8f0951d1729b38b2e392d886a02fedae129
nashs789 <bbock1214@gmail.com>
2023-10-18 22:37:41 +0000

    up-to-date

--- vm/vm.c
+++ vm/vm.c
@@ -282,7 +282,7 @@ supplemental_page_table_copy (struct supplemental_page_table *dst UNUSED, struct
 		} else if (parent_page->operations->type == VM_UNINIT) {  // uninit page인 경우
 			vm_initializer *initializer = parent_page->uninit.init;
 			struct lazy_load_info *aux = (struct lazy_load_info *)malloc(sizeof (struct lazy_load_info));
-			memcpy(aux, parent_page->uninit.aux, sizeof(struct lazy_load_info));//
+			memcpy(aux, parent_page->uninit.aux, sizeof(struct lazy_load_info));
 			if (!vm_alloc_page_with_initializer(type, upage, writable, initializer, aux)) {
 				return false;
 			}
